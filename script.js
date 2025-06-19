2d99ce782d0dd3ceb1e79e9b7ccfd4565ea5a24c
RedPandaSmash <redpandasmash@gmail.com>
2025-06-19 15:22:46 -0400

    done but not icebox

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 async function render(fi, si) {
-    let response = await fetch(`https://rickandmortyapi.com/api/character/${fi},${si}`)
-    let data = await response.json()
-    console.log(data)
-    // Your code goes here
+  let response = await fetch(
+    `https://rickandmortyapi.com/api/character/${fi},${si}`
+  );
+  let data = await response.json();
+  console.log(data);
+  // Your code goes here
 }
 
 render(20, 35);
+
+// DOM create variables from ids of images in html
+let image1 = document.getElementById("firstImage");
+let image2 = document.getElementById("secondImage");
+render(image1, image2);
